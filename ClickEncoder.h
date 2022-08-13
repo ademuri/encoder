c324d0df848792f92939d6062f1b6ff3837d30b3
Adam Demuri <adam.demuri@gmail.com>
2022-08-12 19:33:01 -0600

    Format code

--- ClickEncoder.h
+++ ClickEncoder.h
@@ -14,34 +14,34 @@
 
 // ----------------------------------------------------------------------------
 
-#include <stdint.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <stdint.h>
+
 #include "Arduino.h"
 
 // ----------------------------------------------------------------------------
 
-#define ENC_NORMAL        (1 << 1)   // use Peter Danneger's decoder
-#define ENC_FLAKY         (1 << 2)   // use Table-based decoder
+#define ENC_NORMAL (1 << 1)  // use Peter Danneger's decoder
+#define ENC_FLAKY (1 << 2)   // use Table-based decoder
 
 // ----------------------------------------------------------------------------
 
 #ifndef ENC_DECODER
-#  define ENC_DECODER     ENC_NORMAL
+#define ENC_DECODER ENC_NORMAL
 #endif
 
 #if ENC_DECODER == ENC_FLAKY
-#  ifndef ENC_HALFSTEP
-#    define ENC_HALFSTEP  1        // use table for half step per default
-#  endif
+#ifndef ENC_HALFSTEP
+#define ENC_HALFSTEP 1  // use table for half step per default
+#endif
 #endif
 
 // ----------------------------------------------------------------------------
 
-class ClickEncoder
-{
-public:
+class ClickEncoder {
+ public:
   typedef enum Button_e {
     Open = 0,
     Closed,
@@ -55,7 +55,7 @@ public:
 
   } Button;
 
-public:
+ public:
   ClickEncoder(uint8_t A, uint8_t B, uint8_t BTN = -1,
                uint8_t stepsPerNotch = 1, bool active = LOW);
 
@@ -63,38 +63,28 @@ public:
   int16_t getValue(void);
 
 #ifndef WITHOUT_BUTTON
-public:
+ public:
   Button getButton(void);
 #endif
 
 #ifndef WITHOUT_BUTTON
-public:
-  void setDoubleClickEnabled(const bool &d)
-  {
-    doubleClickEnabled = d;
-  }
+ public:
+  void setDoubleClickEnabled(const bool &d) { doubleClickEnabled = d; }
 
-  const bool getDoubleClickEnabled()
-  {
-    return doubleClickEnabled;
-  }
+  const bool getDoubleClickEnabled() { return doubleClickEnabled; }
 #endif
 
-public:
-  void setAccelerationEnabled(const bool &a)
-  {
+ public:
+  void setAccelerationEnabled(const bool &a) {
     accelerationEnabled = a;
     if (accelerationEnabled == false) {
       acceleration = 0;
     }
   }
 
-  const bool getAccelerationEnabled()
-  {
-    return accelerationEnabled;
-  }
+  const bool getAccelerationEnabled() { return accelerationEnabled; }
 
-private:
+ private:
   const uint8_t pinA;
   const uint8_t pinB;
   const uint8_t pinBTN;
@@ -118,4 +108,4 @@ private:
 
 // ----------------------------------------------------------------------------
 
-#endif // __have__ClickEncoder_h__
+#endif  // __have__ClickEncoder_h__
--- examples/ClickEncoderTest/ClickEncoderTest.ino
+++ examples/ClickEncoderTest/ClickEncoderTest.ino
@@ -6,16 +6,16 @@
 #ifdef WITH_LCD
 #include <LiquidCrystal.h>
 
-#define LCD_RS       8
-#define LCD_RW       9
-#define LCD_EN      10
-#define LCD_D4       4
-#define LCD_D5       5
-#define LCD_D6       6
-#define LCD_D7       7
+#define LCD_RS 8
+#define LCD_RW 9
+#define LCD_EN 10
+#define LCD_D4 4
+#define LCD_D5 5
+#define LCD_D6 6
+#define LCD_D7 7
 
-#define LCD_CHARS   20
-#define LCD_LINES    4
+#define LCD_CHARS 20
+#define LCD_LINES 4
 
 LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 #endif
@@ -23,13 +23,11 @@ LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 ClickEncoder *encoder;
 int16_t last, value;
 
-void timerIsr() {
-  encoder->service();
-}
+void timerIsr() { encoder->service(); }
 
 #ifdef WITH_LCD
 void displayAccelerationStatus() {
-  lcd.setCursor(0, 1);  
+  lcd.setCursor(0, 1);
   lcd.print("Acceleration ");
   lcd.print(encoder->getAccelerationEnabled() ? "on " : "off");
 }
@@ -46,14 +44,14 @@ void setup() {
 #endif
 
   Timer1.initialize(1000);
-  Timer1.attachInterrupt(timerIsr); 
-  
+  Timer1.attachInterrupt(timerIsr);
+
   last = -1;
 }
 
-void loop() {  
+void loop() {
   value += encoder->getValue();
-  
+
   if (value != last) {
     last = value;
     Serial.print("Encoder Value: ");
@@ -65,26 +63,29 @@ void loop() {
     lcd.print(value);
 #endif
   }
-  
+
   ClickEncoder::Button b = encoder->getButton();
   if (b != ClickEncoder::Open) {
     Serial.print("Button: ");
-    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+#define VERBOSECASE(label)  \
+  case label:               \
+    Serial.println(#label); \
+    break;
     switch (b) {
       VERBOSECASE(ClickEncoder::Pressed);
       VERBOSECASE(ClickEncoder::Held)
       VERBOSECASE(ClickEncoder::Released)
       VERBOSECASE(ClickEncoder::Clicked)
       case ClickEncoder::DoubleClicked:
-          Serial.println("ClickEncoder::DoubleClicked");
-          encoder->setAccelerationEnabled(!encoder->getAccelerationEnabled());
-          Serial.print("  Acceleration is ");
-          Serial.println((encoder->getAccelerationEnabled()) ? "enabled" : "disabled");
+        Serial.println("ClickEncoder::DoubleClicked");
+        encoder->setAccelerationEnabled(!encoder->getAccelerationEnabled());
+        Serial.print("  Acceleration is ");
+        Serial.println((encoder->getAccelerationEnabled()) ? "enabled"
+                                                           : "disabled");
 #ifdef WITH_LCD
-          displayAccelerationStatus();
+        displayAccelerationStatus();
 #endif
         break;
     }
-  }    
+  }
 }
-
